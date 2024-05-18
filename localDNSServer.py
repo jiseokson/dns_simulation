3345780dec3395460ab1b8d9cc6558e6c4c7e7f5
Jiseok Son <jiseokson@gmail.com>
2024-05-19 00:44:11 +0900

    fix: DNS 로직 일반화

--- localDNSServer.py
+++ localDNSServer.py
@@ -7,35 +7,33 @@ from config import config
 import cache
 import message
 
-def worker(rrcache: cache.Cache):
+import flag
+
+def worker(rrcache: cache.Cache, recur_flag: flag.Flag, server: str, port: int):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
-        udp_socket.bind(('', config.local.port))
+        udp_socket.bind(('', port))
         while True:
             recevied_query_bytes, address = udp_socket.recvfrom(4092)
             recevied_query = message.decode(recevied_query_bytes)
             reply = recevied_query.copy()
 
-            rrs, is_resolved = rrcache.resolve(recevied_query.name)
             while True:
-                if is_resolved:
+                rrs, is_resolved = rrcache.resolve(recevied_query.name)
+                if is_resolved or not (recur_flag.value or config.root.ip == address[1]):
                     reply.add_answer(*rrs)
                     break
 
                 query = reply.to_query()
-                query.recur_desire = True
-                ns_ip, is_ns_resolved = cache.find_ns_ip(rrs, recevied_query.name)
+                query.recur_desire = recur_flag.value
+                ns_ip, is_ns_resolved = cache.resolve_ip(rrs, recevied_query.name, 'NS')
                 if not is_ns_resolved:
                     break
 
-                query.add_log(config.local.server)
+                query.add_log(server)
                 udp_socket.sendto(query.encode(), ('', config.resolve_port(ns_ip)))
-
                 reply_bytes, _ = udp_socket.recvfrom(4092)
                 reply = message.decode(reply_bytes)
-
                 rrcache.append(*reply.answer)
-                rrs, is_resolved = rrcache.resolve(recevied_query.name)
-                _, is_resolved = cache.find_a_ip(rrs, query.name)
                     
             reply.add_log(config.local.server)
             udp_socket.sendto(reply.encode(), address)
@@ -43,8 +41,9 @@ def worker(rrcache: cache.Cache):
 if __name__ == '__main__':
     rrcache = cache.Cache()
     rrcache.append(cache.RR(config.root.name, config.root.ip, 'A'))
+    recur_flag = flag.Flag(True)
     pattern = re.compile(r'^\s*cache\s*$')
-    worker_thread = threading.Thread(target=worker, args=[rrcache])
+    worker_thread = threading.Thread(target=worker, args=[rrcache, recur_flag, config.local.server, config.local.port])
     worker_thread.start()
     while True:
         prompt = input('>>> ')
