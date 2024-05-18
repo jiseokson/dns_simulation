711bfb940c02be67a9fd32cd220b0d1fa29834bd
Jiseok Son <jiseokson@gmail.com>
2024-05-18 17:50:19 +0900

    add: 캐시 추가 중복 검사 로직, RR 리스트로부터 ip 추출
    - cache에 rr 추가 시 중복 검사, cache에 존재하는 rr들 각각 유일하도록 유지
    - cache에서 resolve된 rr 리스트로부터 실제 ip를 추출하는 find_a_ip, find_ns_ip 함수 정의

--- cache.py
+++ cache.py
@@ -1,6 +1,7 @@
 import re
 import threading
 
+from config import config
 from config import append_all
 
 class RR:
@@ -11,6 +12,10 @@ class RR:
 
     def __str__(self):
         return f'{self.name} : {self.value} ({self.type})'
+    
+    def __eq__(self, obj) -> bool:
+        return isinstance(obj, RR) and \
+            (self.name, self.value, self.type) == (obj.name, obj.value, obj.type)
 
 class Cache:
     def __init__(self, filepath=None):
@@ -24,14 +29,59 @@ class Cache:
             self.rrs
             )
 
-    def append(self, rr):
+    def append(self, *rrs):
         with self.lock:
-            self.rrs.append(rr)
+            for rr in rrs:
+                if rr not in self.rrs: self.rrs.append(rr)
 
     def logs(self):
         with self.lock:
             print('\n'.join(str(rr) for rr in self.rrs))
 
-    def resolve(self, name, type):
+    def resolve(self, name) -> tuple[list[RR], bool]:
         with self.lock:
-            pass
+            ### for test mocking data ###
+            if len(self.rrs) == 1:
+                return [RR('dns.rootDSNService.com', '57.32.9.101', 'A')], False
+            else:
+                return self.rrs[1:], True
+            #############################
+
+def find_a_ip(rrs: list[RR], name: str) -> tuple[str, bool]:
+    try:
+        cname = next(rr.value for rr in rrs if rr.name == name and rr.type == 'CNAME')
+    except StopIteration:
+        cname = None
+    if cname is not None: name = cname
+    try:
+        ip = next(rr.value for rr in rrs if rr.name == name and rr.type == 'A')
+    except StopIteration:
+        return None, False
+    return ip, True
+
+def find_ns_ip(rrs: list[RR], name: str) -> tuple[str, bool]:
+    # resolve authoritative dns
+    domain_name = re.search(r'([a-zA-Z0-9\-]*\.com)', name).string
+    try:
+        ns_ip = next(rr.value for rr in rrs \
+                     if re.match(rf'[a-zA-Z0-9\-]*\.{domain_name}$', rr.name) and rr.type == 'NS')
+    except StopIteration:
+        ns_ip = None
+    if ns_ip: return ns_ip, True
+
+    # resolve comtld dns
+    try:
+        ns_ip = next(rr.value for rr in rrs \
+                     if rr.name == config.comtld.name)
+    except StopIteration:
+        ns_ip = None
+    if ns_ip: return ns_ip, True
+
+    # resolve root dns
+    try:
+        ns_ip = next(rr.value for rr in rrs \
+                     if rr.name == config.root.name)
+    except StopIteration:
+        return None, False
+    
+    return ns_ip, True
