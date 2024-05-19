6e62e39360c55affa8b0368903618805cf975074
Jiseok Son <jiseokson@gmail.com>
2024-05-19 17:55:01 +0900

    refactor: pattern 객체 이름 변경, match 여부 검사 구조 변경

--- client.py
+++ client.py
@@ -7,16 +7,15 @@ import message
 
 if __name__ == '__main__':
     port = int(sys.argv[1])
-    pattern = re.compile(r'^\s*ipaddr\s+([a-zA-Z0-9][a-zA-Z0-9\-\.]*)\s*$')
+    ipaddr_pattern = re.compile(r'^\s*ipaddr\s+([a-zA-Z0-9][a-zA-Z0-9\-\.]*)\s*$')
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
         udp_socket.bind(('', port))
         while True:
             prompt = input('>>> ')
-            match = pattern.match(prompt)
-            if match is None: continue
-            name = match.group(1)
-            query = message.query(name, recur_desire=True)
-            udp_socket.sendto(query.encode(), ('', config.local.port))
-            reply_bytes, _ = udp_socket.recvfrom(4092)
-            reply = message.decode(reply_bytes)
-            print(reply)
+            if match := ipaddr_pattern.match(prompt):
+                name = match.group(1)
+                query = message.query(name, recur_desire=True)
+                udp_socket.sendto(query.encode(), ('', config.local.port))
+                reply_bytes, _ = udp_socket.recvfrom(4092)
+                reply = message.decode(reply_bytes)
+                print(reply)
